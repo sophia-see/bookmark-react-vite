1fb88b75719ee0f725b4d6b0e4eb6abecf124258
Sophia See <sophiasee11@gmail.com>
2024-12-21 02:18:56 +0800

    complete features sections

--- src/components/Features/FeatureDetails.tsx
+++ src/components/Features/FeatureDetails.tsx
@@ -6,6 +6,18 @@ interface FeatureDetailsProps {
 
 export default function FeatureDetails ({selectedFeature}: FeatureDetailsProps) {
     return (
-        <div className="feature__details"></div>
+        <div className="feature__details">
+            <div className="feature__img_container">
+                <img src={selectedFeature.image} alt="" className="full-img" />
+                <div className="img-shadow-left"></div>
+            </div>
+            <div className="content">
+                <div className="heading-2">Bookmark in one click</div>
+                <div className="subheading mb-16">Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.</div>
+                <div className="btn-containers">
+                    <button className="btn primary-btn">More Info</button>
+                </div>
+            </div>
+        </div>
     )
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,15 +2,15 @@ export default function Hero () {
     return (
         <div className="hero__container">
             <div className="hero__img_container">
-                <img src="images/illustration-hero.svg" alt="tablet with bookmark list" />
-                <div className="hero__img_shadow"></div>
+                <img className="full-img" src="images/illustration-hero.svg" alt="tablet with bookmark list" />
+                <div className="img-shadow-right"></div>
             </div>
-            <div className="hero__content">
+            <div className="content">
                 <div className="heading-1">A Simple Bookmark Manager</div>
                 <div className="subheading mt-16 mb-32">A clean and simple interface to organize your favourite websites. Open a new browser tab and see your sites load instantly. Try it for free.</div>
-                <div className="hero__btn_containers">
-                    <button className="btn primary_btn">Get it on Chrome</button>
-                    <button className="btn secondary_btn">Get it on Firefox</button>
+                <div className="btn-containers">
+                    <button className="btn primary-btn">Get it on Chrome</button>
+                    <button className="btn secondary-btn">Get it on Firefox</button>
                 </div>
             </div>
         </div>
